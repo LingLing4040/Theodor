3520772d05e83bdb25d66a214bb657ac9859dd1f
Nikita <filatovnikita1994@yandex.ru>
2021-11-20 22:46:47 +0300

    Сделать мобильную версию

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,21 @@
 import './index.css';
+
+let aboutOffset = document.querySelector('.about');
+
+const buttonUp = document.querySelector('.button-up');
+
+buttonUp.addEventListener('click', () => {
+    window.scrollTo(pageYOffset, 0);
+});
+
+document.addEventListener('scroll', function () {
+    let scrollY = window.pageYOffset;
+
+    if (scrollY >= aboutOffset.offsetTop) {
+        buttonUp.classList.add('button-up_active');
+    }
+
+    if (scrollY <= aboutOffset.offsetTop) {
+        buttonUp.classList.remove('button-up_active');
+    }
+});
